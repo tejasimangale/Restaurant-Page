bbe624c39d4ca63ff51eed428f4cc4819580c79e
Tejasi Mangale <65857144+tejasimangale@users.noreply.github.com>
2020-08-05 22:41:13 +0530

    Add files via upload

---
+++ Js/Main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:true,
+        autoplayTimeout: 3000,
+        dots:true,
+        // navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')]
+    });
+});
